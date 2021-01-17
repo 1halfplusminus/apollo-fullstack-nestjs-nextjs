7f1f854fed38f185aa6c885254285160e9885a35
1halfplusminus <gael.sautron@si2p.nc>
2021-01-17 14:59:46 +1100

    Feat - Logout Fix

--- client/pages/logout.tsx
+++ client/pages/logout.tsx
@@ -1,16 +1,19 @@
+import { useApolloClient } from "@apollo/client";
 import { useRouter } from "next/router";
 import React, { useEffect } from "react";
 import useSWR from "swr";
 import Layout from "../components/layout";
+import { useUser } from "../lib/useUser";
 
 export const LogOut = () => {
-  const { data: user } = useSWR("/api/logout");
-  const router = useRouter();
+  const { data } = useSWR("/api/logout");
+  const client = useApolloClient();
+  const { user } = useUser({ redirectTo: "/login" });
   useEffect(() => {
-    if (user?.isLoggedIn === false) {
-      router.push("/login");
+    if (data?.isLoggedIn == false && user) {
+      client.cache.evict({ id: client.cache.identify(user) });
     }
-  }, [user]);
+  }, [data, user]);
   return <Layout title="Space Explorer" header={<></>} loading={true} />;
 };
 
